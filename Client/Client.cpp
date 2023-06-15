1df421eb32f00ac0a7fd2569ea9354eccd52b5d1
ghostbhd <bhd.a@yandex.com>
2023-06-15 15:47:16 +0100

    irc anass push clean up code

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -12,38 +12,16 @@ Client::~Client()
 {
 }
 
-void Client::setAuth(bool auth)
-{
-    this->_auth = auth;
-}
+void Client::setAuth(bool auth) { this->_auth = auth; }
 
-void Client::setNickname(std::string nickname)
-{
-    this->_nickname = nickname;
-}
+void Client::setNickname(std::string nickname) { this->_nickname = nickname; }
 
-void Client::setPass(std::string pass)
-{
-    this->_pass = pass;
-}
+void Client::setPass(std::string pass) { this->_pass = pass; }
 
-void Client::setUsername(std::string username)
-{
-    this->_username = username;
-}
+void Client::setUsername(std::string username) { this->_username = username; }
 
-bool Client::getAuth()const
-{
-    return (this->_auth);
-}
+bool Client::getAuth() const { return (this->_auth); }
 
-std::string Client::getNickname()const
-{
-    return (this->_nickname);
-}
+std::string Client::getNickname() const { return (this->_nickname); }
 
-std::string Client::getUsername()const
-{
-    return (this->_username);
-}
-
+std::string Client::getUsername() const { return (this->_username); }
